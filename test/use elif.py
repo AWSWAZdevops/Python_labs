b274ac095ef80be5c8a406613e09fa258639ea90
John Doe <john.doe@contoso.com>
2023-03-28 14:16:34 +0100

    use elif

---
+++ test/use elif.py
@@ -0,0 +1,12 @@
+# use elif
+
+age = int(input("enter age :"))
+
+if age >= 18:
+    print("you are in category A")  
+
+elif age >= 16:
+    print("you are in category B")
+
+else:
+    print("you are in category C")
