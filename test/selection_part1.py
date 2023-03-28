7d81bea8918c61bc5e82b0741f0158626fe6c37c
John Doe <john.doe@contoso.com>
2023-03-28 13:55:44 +0100

    lab1

---
+++ test/selection_part1.py
@@ -0,0 +1,10 @@
+age = int(input("enter age :"))
+
+if age >= 18:
+    print("you are in category A")  
+
+if age >= 16:
+    print("you are in category B")
+
+if age < 16:
+    print("you are in category C")
