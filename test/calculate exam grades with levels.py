2aa70226102e0295b58a6588ef5b4bbbe6f9a2bb
John Doe <john.doe@contoso.com>
2023-03-29 09:29:28 +0100

    calculate exam grade

---
+++ test/calculate exam grades with levels.py
@@ -0,0 +1,11 @@
+# calculate exam grades with levels 
+
+exam_mark = int(input("enter a grade:"))
+
+if exam_mark <1:
+    print("fail")
+elif exam_mark >100:
+    print("pass")
+
+if 0 < exam_mark 
+    
