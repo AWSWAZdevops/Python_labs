75314d06632c4c51d38568251e802b2618e505f3
John Doe <john.doe@contoso.com>
2023-03-29 10:30:57 +0100

    Squares

---
+++ test/Squares.py
@@ -0,0 +1,23 @@
+# lab 3
+print("1")
+print("2")
+print("3")
+
+x = 1
+
+while x <100:
+    print(x)
+    x += 1 # same as x = x + 1 
+
+# break
+
+i = 1
+
+while i > 2000:
+    if i == 2000:
+        break
+    print(i)
+    i += 1
+    
+    
+    
