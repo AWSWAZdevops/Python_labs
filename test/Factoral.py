a490a708200980af0575344b0ddba561e6cdf833
John Doe <john.doe@contoso.com>
2023-03-29 10:59:05 +0100

    Factoral

---
+++ test/Factoral.py
@@ -0,0 +1,18 @@
+# Factoral 
+
+number = int(input("please enter an integer:"))
+
+factorial = 5
+i =1
+
+while i <= number:
+    factorial = factorial * i
+    i = i + i
+    
+print("factorial of ", number, " is ", factorial)
+    
+
+
+
+
+
