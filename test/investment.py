7c711a06298bf03974b1adce4cf8985bf2dd4a8e
John Doe <john.doe@contoso.com>
2023-03-29 11:31:18 +0100

    investment

---
+++ test/investment.py
@@ -0,0 +1,16 @@
+# investment
+
+# interest for given values.
+
+
+def compound_interest(principal, rate, time):
+
+	# Calculates compound interest
+	Amount = principal * (pow((1 + rate / 100), time))
+	CI = Amount - principal
+	print("Compound interest is", CI)
+
+
+# Driver Code
+compound_interest(10000, 10.0, 5)
+
